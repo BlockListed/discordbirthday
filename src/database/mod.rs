97763fbc8a705c12e8338e8a8f5a9fcefc761cf2
BlockListed <44610569+BlockListed@users.noreply.github.com>
2022-07-27 22:38:14 +0200

    made database static variable Db-agnostic and started moving all interactions with db to db_independant module

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -2,17 +2,25 @@ use diesel::prelude::*;
 use once_cell::sync::Lazy;
 use std::sync::Mutex;
 
+pub mod db_independant;
 pub mod statements;
+pub enum DbType {
+    Postgres(PgConnection),
+    Sqlite(SqliteConnection),
+}
 
-#[cfg(all(feature = "postgres", not(features = "sqlite")))]
-pub static DB: Lazy<Mutex<PgConnection>> = Lazy::new(|| {
-    Mutex::new(PgConnection::establish(&std::env::var("DATABASE_URL").unwrap()).unwrap())
-});
-
-#[cfg(feature = "sqlite")]
-pub static DB: Lazy<Mutex<SqliteConnection>> = Lazy::new(|| {
-    Mutex::new(SqliteConnection::establish(
-        &std::env::var("DATABASE_URL").unwrap(),
-    ))
-    .unwrap()
+pub static DB: Lazy<Mutex<DbType>> = Lazy::new(|| {
+    match std::env::var("DATABASE_TYPE")
+        .expect("Database type not provided")
+        .as_str()
+    {
+        "postgres" => Mutex::new(DbType::Postgres(
+            PgConnection::establish(&std::env::var("DATABASE_URL").unwrap())
+                .expect("Failed to initialise postgres DB."),
+        )),
+        "sqlite" => Mutex::new(DbType::Sqlite(
+            SqliteConnection::establish(&std::env::var("DATABASE_URL").unwrap())
+                .expect("Failed to initialise sqlite DB."),
+        )),
+    }
 });
