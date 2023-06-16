3686a1079ce91e3a596f9b213244e91ca30425c0
BlockListed <44610569+BlockListed@users.noreply.github.com>
2023-06-16 15:31:08 +0200

    use proper chrono methods instead of parsing strings

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,32 +1,16 @@
 use crate::database;
-use chrono::NaiveDate;
-use chrono::Utc;
+use chrono::{Utc, NaiveDate, Datelike};
 use nanoid::nanoid;
 pub mod discord;
 pub mod macros;
 
 pub fn date_as_year_zero(date: NaiveDate) -> NaiveDate {
-    date_as_year(date, 0)
+    date.with_year(0).unwrap()
 }
 
 pub fn date_as_year_today(date: NaiveDate) -> NaiveDate {
-    let year = Utc::today()
-        .naive_local()
-        .format("%Y")
-        .to_string()
-        .parse::<i32>()
-        .unwrap();
-    date_as_year(date, year)
-}
-
-pub fn date_as_year(date: NaiveDate, year: i32) -> NaiveDate {
-    let date = date
-        .format("%m-%d")
-        .to_string()
-        .split('-')
-        .map(|x| x.parse::<u32>().unwrap())
-        .collect::<Vec<u32>>();
-    NaiveDate::from_ymd(year, date[0], date[1])
+    let year = Utc::now().year();
+    date.with_year(year).unwrap()
 }
 
 pub fn gen_id() -> String {
